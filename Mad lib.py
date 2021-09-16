5dfe4a9030323063e3f5ffd594540688b66e91ee
vnativid <86301156+vnativid@users.noreply.github.com>
2021-09-16 12:51:23 -0400

    Add files via upload

---
+++ Mad lib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 11:46:35 2021
+
+@author: vincenatividad
+"""
+
+# string concatenation (putting strings together)
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib =  f"Computer Programming is so {adj}! It makes me so excited all the time\
+ because I love to {verb1}. Stay hydrated and {verb2}\
+ like you are {famous_person}!"
+
+print(madlib)
